82bef48b5f044c5abbe6af30125611ef1ecc2e2b
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-20 19:19:03 +0700

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -23,6 +23,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
+            await cmd.reply_text("Anda Dilarang Menggunakan Bot Ini 🥵", quote=True)
             return
     await cmd.continue_propagation()
